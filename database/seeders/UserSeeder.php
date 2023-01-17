b72fa6b9eae126897efb21ceac0761e9b168d5ab
Tyson VanOverhill <tyson.vanover@gmail.com>
2023-01-17 12:07:48 -0800

    Database seeder with default user added

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        User::factory()->create([
+            'id' => 1,
+            "name"=>env('INITIAL_USER_NAME'),
+            "email"=> env('INITIAL_USER_EMAIL'),
+            "email_verified_at"=> now(),
+            "password"=>  env('INITIAL_USER_PASSWORDHASH'),
+            "created_at"=> now(),
+            "updated_at"=> now(),
+        ]);
+    }
+}
