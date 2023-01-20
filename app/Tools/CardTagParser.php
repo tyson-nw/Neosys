5336639f767175c88111c16edc0c185922dddfd7
Tyson VanOverhill <tyson.vanover@gmail.com>
2023-01-20 11:31:44 +0000

    Regex Complete

--- app/Tools/CardTagParser.php
+++ app/Tools/CardTagParser.php
@@ -5,7 +5,7 @@ namespace App\Tools;
 class CardTagParser{
     public function __invoke(string $string) : string{
         $out = [];
-        preg_match_all("/\[\[\#([A-z]*)\]\]/", $string,$out);
+        preg_match_all("/\[\[\#([A-z \d]*)\]\]/", $string,$out);
         for($n=0 ; isset($out[1][$n]) ; $n++){
             $string = str_replace($out[0][$n], "<a href='#' card='{$out[1][$n]}'>{$out[1][$n]}</a>",$string);
         }
